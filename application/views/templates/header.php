d15e70b4dbfdb7b3e9e48422017598d2742b955b
Val Ezekiel Baga <itszikeyy14@gmail.com>
2017-08-15 18:57:24 +0800

    Changed link tag

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -3,11 +3,9 @@
 <head>
 	<title><?php echo $title; ?></title>
 
-	<?php
-		echo link_tag('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css', 'stylesheet', 'text/css');
-		echo link_tag('assets/css/custom.css', 'stylesheet', 'text/css');
-		echo link_tag('assets/sweetalert-master/dist/sweetalert.css');
-	?>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/custom.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/sweetalert-master/dist/sweetalert.css">
 		
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
